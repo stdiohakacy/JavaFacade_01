83ff2f6068570c46408b8760440d149069e996ec
Duy <huynhduytam2210@gmail.com>
2020-01-09 15:52:14 +0700

    example

---
+++ AccountService.java
@@ -0,0 +1,8 @@
+package structual.Facade._01;
+
+public class AccountService {
+
+	public void getAccount(String email) {
+		System.out.println("Getting the account of " + email);
+	}
+}
---
+++ SmsService.java
@@ -0,0 +1,7 @@
+package structual.Facade._01;
+
+public class SmsService {
+	public void sendSMS(String mobilePhone) {
+		System.out.println("Sending an mesage to " + mobilePhone);
+	}
+}
